378f9745686ae269074c150dcc16afb939fc178f
AZ <amiraelzaian204@gmail.com>
2026-01-07 07:30:27 +0200

    feat: integrate json-server for city data management
    - Added json-server as a dependency and configured a script to run it.
    - Removed the old cities.json file from the public directory and created a new one in the data directory.
    - Implemented fetching of city data in the App component using useEffect.
    - Created CityList and CityItem components to display the list of cities.
    - Added CountryList component to display unique countries from the city data.
    - Updated Sidebar component to remove unnecessary text.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ function Sidebar() {
       <Logo />
       <AppNav />
       <Outlet />
-      <p className="">List of cities</p>
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
